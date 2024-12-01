641d2ec2092436ee3d91677ddb168660ced0b9ad
Wise Old Duck <166949486+JuggerboyJones@users.noreply.github.com>
2024-12-01 16:50:21 +0000

    Add files via upload
    key files

---
+++ Backend/db.php
@@ -0,0 +1,17 @@
+<?php
+
+$db_host = 'localhost';
+$db_name = 'retro_xchange';
+$username = 'root';
+$password = '';
+
+try {
+	$db = new PDO("mysql:dbname=$db_name;host=$db_host", $username, $password);
+	echo("Successfully connected to the  retroX database.<br>");
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $ex) {
+	echo("Failed to connect to the retroX database.<br>");
+	echo($ex->getMessage());
+	exit;
+}
+?>
