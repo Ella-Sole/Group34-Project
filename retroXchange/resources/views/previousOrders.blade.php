cad96915cf8a0dc1d521d3add8093daad0b6d2fd
Sidra <sidraaj0401@gmail.com>
2024-12-07 21:57:03 +0000

    Updated checkout page html
    updated the view with the checkoutpage html from the frontend, and added in the new header and footer

--- retroXchange/resources/views/previousOrders.blade.php
+++ retroXchange/resources/views/previousOrders.blade.php
@@ -3,7 +3,7 @@
 <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width. initial-scale = 1.0">
-        <title>Games</title>
+        <title>Previous Orders</title>
         <link rel="stylesheet" href="{{asset('/style.css')}}"> <!-- Link to CSS File-->
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
