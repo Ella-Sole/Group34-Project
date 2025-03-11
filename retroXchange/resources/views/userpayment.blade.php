53b03db3a89fff301451f7acc9b8d0e2dadab30c
Ella Sole <230111497@aston.ac.uk>
2025-03-11 15:03:31 +0000

    tidyed up user payment page code

--- retroXchange/resources/views/userpayment.blade.php
+++ retroXchange/resources/views/userpayment.blade.php
@@ -1,11 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Profile Settings</title>
-</head>
+<x-head>
+    @section('title', 'User Payment')
+</x-head>
 <body>
+     <x-header-navbar> </x-header-navbar>
     <div class="container">
         <h2 class="title">Profile Settings</h2>
         <input class="input-field" id="name" type="text" placeholder="Full Name">
@@ -24,37 +23,6 @@
         <button class="button button-fail" onclick="removeProfile()">Remove Profile</button>
     </div>
 
-    <script>
-        function updateProfile() {
-            const name = document.getElementById("name").value;
-            const email = document.getElementById("email").value;
-            if (name && email) {
-                alert("Profile updated");
-            } else {
-                alert("Please enter name and email.");
-            }
-        }
 
-        function addPayment() {
-            const paymentList = document.getElementById("payment-list");
-            const newPayment = document.createElement("div");
-            newPayment.classList.add("payment-option");
-            newPayment.innerHTML = '<span>MasterCard - **** 5824</span> <button class="button button-fail" onclick="deletePayment(this)">Delete</button>';
-            paymentList.appendChild(newPayment);
-        }
-
-        function deletePayment(button) {
-            button.parentElement.remove();
-        }
-
-        function removeProfile() {
-            if (confirm("Do you want to delete this profile?")) {
-                alert("Profile deleted");
-                document.getElementById("name").value = "";
-                document.getElementById("email").value = "";
-                document.getElementById("payment-list").innerHTML = "";
-            }
-        }
-    </script>
 </body>
 </html>
