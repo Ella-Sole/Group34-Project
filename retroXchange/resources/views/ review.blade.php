077c3490088f30c2b266797f7b02bff3620fa40c
Ella Sole <230111497@aston.ac.uk>
2025-03-11 14:59:22 +0000

    Update  review.blade.php
    added onclick for review function/ general tidying

--- retroXchange/resources/views/ review.blade.php
+++ retroXchange/resources/views/ review.blade.php
@@ -14,29 +14,13 @@
             <input type="text" id="name" name="name" required><br><br>
             <label for="review">Review</label><br>
             <textarea id="review" name="review" required></textarea><br><br>
-            <button type="submit">Submit</button>
+            <button type="submit" onclick = "review()">Submit</button>
         </form>
 
 <h1>  Reviews   </h1>
 <p> This page is dedicated to customers who have purchased products from us, these reviews are checked legitmately to make sure people can buy our products  </p>
 <p> As seen below we have an outline of what the customers think of the games </p>
 <div id="reviews"></div>
-<script>
-    document.getElementById("review").addEventListener("submit", function(event) {
-        event.preventDefault();
-        
-        let name = document.getElementById("name").value;
-        let review = document.getElementById("review").value;
-        
-        if (name && review) {
-            let reviewDiv = document.createElement("div");
-            reviewDiv.classList.add("review");
-            reviewDiv.innerHTML = `<strong>${name}:</strong> <p>${review}</p>`;
-        }
-        
-        document.getElementById("reviewForm").reset();
-    });
-</script>
 
 <hr> 
 
