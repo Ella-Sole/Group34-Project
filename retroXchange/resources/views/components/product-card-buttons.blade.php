7776d4d985fb3077cae4ea89e5cf127153544ea4
Vivienen1 <230416750@aston.ac.uk>
2025-03-23 23:56:27 +0000

    Create product-card-buttons.blade.php

---
+++ retroXchange/resources/views/components/product-card-buttons.blade.php
@@ -0,0 +1,6 @@
+<button class="pre-btn">
+            <img src="{{ asset('images/arrow-button.png') }}" alt="back-button" style="height: 25px; width: 25px;">
+            </button>
+            <button class="nxt-btn">
+            <img src="{{ asset('images/arrow-button.png') }}" alt="fwd-button" style="height: 25px; width: 25px;">
+            </button>
