06e20ebad07c2fd8c4371b0243fe05e49cee8647
Vivienen1 <230416750@aston.ac.uk>
2025-03-23 23:59:42 +0000

    Update productListingPage.blade.php

--- retroXchange/resources/views/productListingPage.blade.php
+++ retroXchange/resources/views/productListingPage.blade.php
@@ -6,8 +6,6 @@
 
 <body class = "games-page-layout background">
     <x-header-navbar> </x-header-navbar>
-
-
     <main>
         <!--message for item being successfully added to basket -->
         @if (session()->has('success'))
@@ -20,49 +18,14 @@
         @endif
 
         <!-- Gaming products  -->
-        <section class="product">
+        <section class="products">
             <h2 class="product-category">Games</h2>
-
+            <x-product-card-buttons />
             <!-- class that holds all products-->
             <div class="product-container">
-
-                <!-- Products -->
-                <!-- class that holds individual products-->
-                <!--loop through the array $products, each element being a variable $product-->
-                @foreach ($products as $product)
-                    <div class="product-card">
-                        <div class="product-image">
-                            <!--make image location point to image named with same item ID-->
-                            <a href = "{{ url('productview/' . $product->item_id) }}">
-                                <img src="{{ url('https://m.media-amazon.com/images/I/' . $product->item_image . '.jpg') }}"
-                                    class="product-thumbnail" alt="item-image">
-                            </a>
-                        </div>
-
-                        <div class="product-info"> <!--product info from database -->
-                            <p class="product-brand">{{ $product['category'] }}</p> <!-- Category -->
-                            <h2 class="product-name">{{ $product['item_name'] }}</h2>
-                            <p class="product-rating">&#9733 &#9733 &#9733 &#9733 &#9733 5.0 (2)</p>
-                            <h2 class="price">£{{ $product['item_price'] }}</h2> <!-- price -->
-
-                            @if ($product['item_stock'] <= 0)
-                                <!--if out of stock-->
-                                <p>OUT OF STOCK</p>
-                            @else
-                                <!--can only add to basket if in stock-->
-                                <!--make the button send this products info into basket-->
-                                <!--add item to basket when button is clicked, passing in item_id (**add confirmation message to user)-->
-                                <a href = "{{ url('addToBasket/' . $product->item_id) }}">
-                                    <button type = "submit" class="product-listing-basket-icon"><img
-                                            src="{{ asset('images/basket-icon.png') }}" alt="add-to-basket"
-                                            style="height: 25px; width: 25px;"></button>
-                                </a>
-                            @endif
-
-                        </div>
-                    </div>
+                @foreach ($products->whereIn('category', ['ps3', 'ps4', 'xbox one', 'xbox 360', 'switch']) as $product)
+                <x-product-card :product="$product"/>
                 @endforeach
-
             </div>
         </section>
     </main>
