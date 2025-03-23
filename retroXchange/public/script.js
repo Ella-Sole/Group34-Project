256c48db83e3888641c4df7ff41c3a5bcacb36a9
Vivienen1 <230416750@aston.ac.uk>
2025-03-23 13:25:32 +0000

    Update script.js

--- retroXchange/public/script.js
+++ retroXchange/public/script.js
@@ -107,5 +107,22 @@ for (i = 0; i < dropdown.length; i++) {
 }
 
 function sidemenuFunction() {
-    document.querySelector(".sidemenu").classList.toggle("show");
-}
+document.querySelector(".sidemenu").classList.toggle("show");
+  }
+
+      function toggleDarkMode() {
+        document.body.classList.toggle("dark-mode");
+    
+        if (document.body.classList.contains("dark-mode")) {
+            localStorage.setItem("darkMode", "enabled");
+        } else {
+            localStorage.setItem("darkMode", "disabled");
+        }
+    }
+    
+    window.onload = function () {
+        if (localStorage.getItem("darkMode") === "enabled") {
+            document.body.classList.add("dark-mode");
+        }
+    };
+
