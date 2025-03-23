63d34e7ebffb330fcf89e6b65a204f2a26bb1955
Vivienen1 <230416750@aston.ac.uk>
2025-03-23 11:00:20 +0000

    Update homepage.blade.php

--- retroXchange/resources/views/homepage.blade.php
+++ retroXchange/resources/views/homepage.blade.php
@@ -9,20 +9,26 @@
     <x-header-navbar> </x-header-navbar>
 
     <main>
-        <!--message for item being successfully added to basket -->
+        <!--for debugging-->
+        @if (Auth::check())
+            <p> authenticated </p>
+        @else
+            <p> not authenticated </p>
+        @endif
+
+        <!--message that will display either successful login/logout or item added to basket -->
         @if (session()->has('success'))
-            <h2 style="color:red" align="center"> {{ session('success') }}</h2>
+            <p style="color:red"> {{session('success')}}</p>
         @endif
 
-        <!--error message-->
-        @if (session()->has('error'))
-            <h2 style="color:red" align="center"> {{ session('error') }}</h2>
-        @endif
-
-        <!-- Gaming products  -->
-        <section class="product">
+        <section class="products">
             <h2 class="product-category">Games</h2>
-
+            <button class="pre-btn">
+            <img src="{{ asset('images/arrow-button.png') }}" alt="back-button" style="height: 25px; width: 25px;">
+            </button>
+            <button class="nxt-btn">
+            <img src="{{ asset('images/arrow-button.png') }}" alt="fwd-button" style="height: 25px; width: 25px;">
+            </button>
             <!-- class that holds all products-->
             <div class="product-container">
 
@@ -40,29 +46,29 @@
                         </div>
 
                         <div class="product-info"> <!--product info from database -->
-                            <p class="product-brand">{{ $product['category'] }}</p> <!-- Category -->
-                            <h2 class="product-name">{{ $product['item_name'] }}</h2>
-                            <p class="product-rating">&#9733 &#9733 &#9733 &#9733 &#9733 5.0 (2)</p>
-                            <h2 class="price">£{{ $product['item_price'] }}</h2> <!-- price -->
+                            <div class="product-wrap">
+                                <p class="product-brand">{{ $product['category'] }}</p> <!-- Category -->
+                                <p class="product-rating">&#9733 5.0</p>
+                            </div>
+                            <h2 class="product-short-description">{{ $product['item_name'] }}</h2>
+                            <div class="product-wrap">
+                                <span class="price">£{{ $product['item_price'] }}</span> <!-- price -->
 
-                            @if ($product['item_stock'] <= 0)
-                                <!--if out of stock-->
-                                <p>OUT OF STOCK</p>
-                            @else
-                                <!--can only add to basket if in stock-->
-                                <!--make the button send this products info into basket-->
-                                <!--add item to basket when button is clicked, passing in item_id (**add confirmation message to user)-->
-                                <a href = "{{ url('addToBasket/' . $product->item_id) }}">
-                                    <button type = "submit" class="product-listing-basket-icon"><img
-                                            src="{{ asset('images/basket-icon.png') }}" alt="add-to-basket"
-                                            style="height: 25px; width: 25px;"></button>
-                                </a>
-                            @endif
-
+                                @if ($product['item_stock'] == 0)
+                                    <!--if out of stock-->
+                                    <p>OUT OF STOCK</p>
+                                @else <!--can only add to basket if in stock-->
+                                    <!--make the button send this products info into basket-->
+                                    <!--add item to basket when button is clicked, passing in item_id (**add confirmation message to user)-->
+                                    <a href = "{{ url('addToBasket/' . $product->item_id) }}">
+                                        <button type = "submit" class="basket"><img src="{{ asset('images/basket-icon.png') }}"
+                                                alt="add-to-basket" style="height: 25px; width: 25px;"></button>
+                                    </a>
+                                @endif
+                            </div>
                         </div>
                     </div>
                 @endforeach
-
             </div>
         </section>
     </main>
